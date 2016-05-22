2822ac7adef02314e513ca8c4478a5f4c1413e09
visa4 <antonino.visalli@gmail.com>
2016-05-22 16:57:14 +0200

    Fix composer and add entity class

---
+++ src/Entity/NightClubEntity.php
@@ -0,0 +1,12 @@
+<?php
+namespace Strapieno\NightClubCover\Model\Entity;
+
+use Strapieno\Utils\Model\Entity\ImgMan\ImgManEntity;
+
+/**
+ * Class NightClubEntity
+ */
+class NightClubEntity extends ImgManEntity
+{
+
+}
